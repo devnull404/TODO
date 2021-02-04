2412b3d3e5997b124fc6695b599693874e37ef13
francisco <linux.development404@gmail.com>
2021-02-04 16:47:49 -0600

    Fixing style

--- JS/main.js
+++ JS/main.js
@@ -4,8 +4,10 @@ document.addEventListener("mousemove", function(e) {
 
 function drawer() {
     let red = Math.floor(Math.random()*254).toString();
-    let green = Math.floor(Math.random()*40).toString();
+    red = (10).toString()
+    let green = Math.floor(Math.random()*254).toString();
     let blue = Math.floor(Math.random()*254).toString();
+    blue = (10).toString()
     let inProp = "rgb(" + red + ", " + green + ", " + blue + ")";
     document.getElementsByClassName("decorationBar")[1].style.backgroundColor = inProp;
 }
