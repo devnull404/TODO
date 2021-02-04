3a18e43e2562f224924cd36a032252084430b157
francisco <linux.development404@gmail.com>
2021-02-04 09:19:37 -0600

    Fixing wrong view at index html

--- JS/main.js
+++ JS/main.js
@@ -4,8 +4,8 @@ document.addEventListener("mousemove", function(e) {
 
 function drawer() {
     let red = Math.floor(Math.random()*254).toString();
-    let green = Math.floor(Math.random()*254).toString();
-    let blue = Math.floor(Math.random()*50).toString();
+    let green = Math.floor(Math.random()*40).toString();
+    let blue = Math.floor(Math.random()*254).toString();
     let inProp = "rgb(" + red + ", " + green + ", " + blue + ")";
     document.getElementsByClassName("decorationBar")[1].style.backgroundColor = inProp;
 }
