d181f1007aaf5411a7921340adb34bbe47a081aa
Francisco <ecoli404@icloud.com>
2021-02-06 18:38:43 -0600

    Adding delete and add functions to task js

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,5 @@
+var countRemovingID = 1
+
 document.addEventListener("mousemove", function(e) {
     drawer(e)
 });
@@ -12,5 +14,51 @@ function drawer(event) {
     document.getElementsByClassName("decorationBar")[1].style.backgroundColor = inProp;
 }
 
-document.getElementById("myapp").innerHTML += '<input type="text" placeholder="New task">'
-document.getElementById("myapp").innerHTML += '<button>add</button>'
+document.getElementById("myapp").innerHTML += '<input id="task" type="text" placeholder="New task">'
+document.getElementById("myapp").innerHTML += '<button onclick=getTask()>add</button>'
+document.getElementsByTagName("input")[0].addEventListener("keyup", function (e) {
+    console.log(e.key)
+    if (e.key === "Enter") {
+        document.getElementsByTagName("button")[0].click()
+    }
+});
+function getTask() {
+    let inputNode = document.getElementById("task");
+    let taskText = null;
+    if (inputNode.value != "") {
+        taskText = inputNode.value;
+        inputNode.value = "";
+        inputNode.placeholder = "New Task";
+        createTask(taskText);
+        document.getElementById("task").style.border = "none"
+    } else {
+        inputNode.placeholder = "¡The input cannot be empty!";
+        document.getElementById("task").style.border = "2px solid red"
+    }
+}
+
+function createTask(str) {
+    container = document.getElementById("appContainer")
+    let taskContainer = document.createElement("div");
+    taskContainer.className = "taskContainer";
+    let checkBox  = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.class = "check"
+    let textTask = document.createElement("div");
+    textTask.innerHTML = str;
+    let deleteButton = document.createElement("button")
+    deleteButton.className = "delButton"
+    deleteButton.id = "r" + countRemovingID.toString()
+    deleteButton.onclick = function onclick(event) {test(this.id)}
+    deleteButton.innerHTML = "&times"
+    container.appendChild(taskContainer)
+    container.childNodes[container.childNodes.length-1].appendChild(checkBox)
+    container.childNodes[container.childNodes.length-1].appendChild(textTask)
+    container.childNodes[container.childNodes.length-1].appendChild(deleteButton)
+    countRemovingID += 1
+}
+
+function test(something) {
+    console.log(something)
+    document.getElementById(something).parentNode.remove()
+}
