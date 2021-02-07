13fc1eb4df032a2f530863bf0976e56df02677f7
francisco <linux.development404@gmail.com>
2021-02-07 01:17:26 -0600

    fixing html elements outside of myapp function

--- JS/main.js
+++ JS/main.js
@@ -66,3 +66,21 @@ function test(something) {
 function lineT(something) {
     something.childNodes[1].style.textDecoration === "" ? something.childNodes[1].style.textDecoration = "line-through" : something.childNodes[1].style.textDecoration = ""
 }
+
+
+document.body.innerHTML = `<header>
+<nav>
+    <img src="https://img.icons8.com/plasticine/2x/pencil.png" alt="TODO Logo">
+    <a href="#body">Inicio</a>
+</nav>
+<div class="decorationBar"></div>
+<div class="decorationBar"></div>
+</header>` + document.body.innerHTML
+
+document.getElementById("body").innerHTML = `<section class="infoBubble">
+<h1>2DO.com</h1>
+<p>
+    En esta página podrás realizar una lista de tareas para tu día a día ¡Esperamos
+    que te sea de mucha utilidad!
+</p>
+</section>` + document.getElementById("body").innerHTML
